861cbb9c17bb821059c02eedafecc36e46b06f70
Zhiyi Xu <yzxu@ucdavis.edu>
2019-02-26 02:01:30 -0800

    init

---
+++ AboutUs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutUs
+//
+//  Created by Zhiyi Xu on 2/26/19.
+//  Copyright © 2019 Zhiyi Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
