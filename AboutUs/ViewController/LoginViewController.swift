d10ba68d8a969d7c07b8244f21dc1b314c461871
Zhiyi Xu <yzxu@ucdavis.edu>
2019-03-06 20:39:13 -0800

    change to email

--- AboutUs/ViewController/LoginViewController.swift
+++ AboutUs/ViewController/LoginViewController.swift
@@ -11,23 +11,12 @@ import Firebase
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var verify: UIButton!
-    @IBOutlet weak var numberField: UITextField!
-    
-    @IBAction func verifyOntap(_ sender: Any) {
-//        PhoneAuthProvider.provider().verifyPhoneNumber("+15306018718", uiDelegate: nil) { (verificationID, error) in
-//            if let error = error {
-////                self.showMessagePrompt(error.localizedDescription)
-//                return
-//            }
-//            // Sign in using the verificationID and the code sent to the user
-//            // ...
-//        }
-    }
+    @IBOutlet weak var signin: UIButton!
+    @IBOutlet weak var signup: UIButton!
     
     func viewInit() {
-        verify.layer.cornerRadius = 20
-//        numberField.setBottomBorder()
+        signin.layer.cornerRadius = 20
+        signup.layer.cornerRadius = 20
     }
     
     override func viewDidLoad() {
@@ -36,15 +25,3 @@ class LoginViewController: UIViewController {
     }
 }
 
-extension UITextField {
-    func setBottomBorder() {
-        self.borderStyle = .none
-        self.layer.backgroundColor = UIColor.clear.cgColor
-        
-        self.layer.masksToBounds = false
-        self.layer.shadowColor = UIColor.white.cgColor
-        self.layer.shadowOffset = CGSize(width: 0.0, height: -1.0)
-        self.layer.shadowOpacity = 1.0
-        self.layer.shadowRadius = 0.0
-    }
-}
